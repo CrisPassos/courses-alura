3d21a90dfe7111fcbbfd1c7a5dc4f0b499cc3f65
Cristiana Passos <cristiana.passos@bee-eng.pt>
2021-04-03 09:38:10 +0100

    first commit for 6 project

---
+++ 06. React - Function Components uma abordagem moderna/form-cadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Switch, Button, TextField, FormControlLabel } from "@material-ui/core";
+
+function FormularioCadastro() {
+  return (
+    <form>
+      <TextField
+        id="name"
+        label="Name"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+
+      <TextField
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+
+      <FormControlLabel
+        label="Promoções"
+        control={<Switch name="promoções" defaultChecked color="primary" />}
+      />
+
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch name="novidades" defaultChecked color="primary" />}
+      />
+
+      <Button variant="contained" color="primary" type="submit">
+        Cadastrar
+      </Button>
+    </form>
+  );
+}
+
+export default FormularioCadastro;
