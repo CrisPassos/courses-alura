b881854b6dc76891f370b8b2c5b47369481a57e2
Cristiana Passos <cristiana.passos@bee-eng.pt>
2021-04-08 20:38:26 +0100

    more one commit

--- 08. React - Router - Navegação em uma SPA/petshop/src/App.jsx
+++ 08. React - Router - Navegação em uma SPA/petshop/src/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import "./assets/css/base/base.css";
 import Home from "./paginas/Home";
 import Sobre from "./paginas/Sobre";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home />
-      <Sobre />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/sobre">
+            <Sobre />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
